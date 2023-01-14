fa1344bb8e94a614ee095ca6090a00f0d030756f
Firdavs Jalolov <jalolov.firdavs0809@gmail.com>
2023-01-14 14:18:14 +0500

    sensitive data handled

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -113,10 +113,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'devsearch',
-        'USER':"postgres",
-        'PASSWORD':'fi77av77ek',
+        'USER':env.str("USER"),
+        'PASSWORD':env.str("DB_PASSWORD"),
         'PORT':"5432",
-        'HOST':'database-1.cljtfi6np5vp.ap-northeast-1.rds.amazonaws.com',
+        'HOST':env.str("HOST"),
     }
 }
 
@@ -156,9 +156,9 @@ CORS_ALLOW_ALL_ORIGINS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER='udemytogether204@gmail.com'
+EMAIL_HOST_USER=env.str("EMAIL_HOST_USER")
 EMAIL_USE_TLS=True
-EMAIL_HOST_PASSWORD='zhplvuyvjjltyhmg'
+EMAIL_HOST_PASSWORD=env.str("EMAIL_HOST_PASSWORD")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
@@ -182,4 +182,4 @@ AWS_S3_FILE_OVERWRITE = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = 'devsearch-bucket-frank'
+AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
